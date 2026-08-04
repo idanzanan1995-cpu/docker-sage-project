7c8fb67478c7297373240cd70694c59c969f5868
Idan Anan <idanz.anan1995@gmail.com>
2026-08-04 16:50:46 +0300

    Add Service and ConfigMap support

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
+import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
 
 HOST = "0.0.0.0"
 PORT = 8080
-
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"Hello from Kubernetes mada fakar ! Version 4.\n")
 
+        env = os.getenv("ENV", "unknown")
+
+        message = f"Hello from Kubernetes! Version 5.\nEnvironment: {env}\n"
+
+        self.wfile.write(message.encode())
 
 if __name__ == "__main__":
     print(f"Server starting on {HOST}:{PORT}")
     HTTPServer((HOST, PORT), SimpleHandler).serve_forever()
-
-
-
 
